a96001006907fd962c6beae9d350bf0da24ee008
Hieu Do <hieu271101@gmail.com>
2025-02-03 23:13:24 +0700

    add sidebar

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,10 @@
+import styles from "./css/SideBar.module.css";
+export default function SideBar() {
+    return (
+        <>
+            <div >
+                <SideBar className={styles.container}/>
+            </div>
+        </>
+    );
+}
