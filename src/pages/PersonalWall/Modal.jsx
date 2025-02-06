a290fde108f4d85d088fb718a3b54a6d1018964b
khanhisreal <nguyenhiu2k2@gmail.com>
2025-02-06 23:03:15 +0700

    update PersonalWall

---
+++ src/pages/PersonalWall/Modal.jsx
@@ -0,0 +1,20 @@
+import styles from "./PersonalWall.module.css";
+import PropTypes from "prop-types";
+
+export default function Modal({ image, caption, handleModal }) {
+  return (
+    <div id={styles.myModal} className={styles.modal}>
+      <span className={styles.close} onClick={handleModal}>
+        x
+      </span>
+      <img src={image} className={styles.modalContent} id={styles.img01} />
+      <div id={styles.caption}>{caption}</div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  image: PropTypes.node.isRequired,
+  caption: PropTypes.string.isRequired,
+  handleModal: PropTypes.func.isRequired,
+};
--- src/pages/PersonalWall/PersonalWall.jsx
+++ src/pages/PersonalWall/PersonalWall.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 import styles from "./PersonalWall.module.css";
 import dummyAvatar from "../../assets/images/Pages/dummy_avatar.png";
-import { useState } from "react";
+import Modal from "./Modal";
 
 import { FaCameraRetro, FaImages } from "react-icons/fa";
 
@@ -35,17 +37,11 @@ export default function PersonalWall() {
           </a>
           {/* The modal */}
           {isModalOpen && (
-            <div id={styles.myModal} className={styles.modal}>
-              <span className={styles.close} onClick={closeModal}>
-                x
-              </span>
-              <img
-                src={modalImageSrc}
-                className={styles.modalContent}
-                id={styles.img01}
-              />
-              <div id={styles.caption}>{modalCaption}</div>
-            </div>
+            <Modal
+              image={modalImageSrc}
+              caption={modalCaption}
+              handleModal={closeModal}
+            />
           )}
         </div>
         <a href="#">
